0252ceb0b7123ba12aa51a5b71e4e0a1bc7880d0
diduk001 <didurenkos@gmail.com>
2021-11-04 23:28:56 +0300

    fixed text

--- decrypt.py
+++ decrypt.py
@@ -4,13 +4,11 @@ ALPHABET = bytes(ascii_letters + digits + punctuation, encoding="utf-8")
 ALPHABET_SZ = len(ALPHABET)
 BLOCK_SZ = 16
 
-ciphertext = b'DECFmmDECllEDFmm\x05`.\x08\n\x00\x05`.\x0b\x06\x00_ \n\x07oSUW:zWS1S:yTVP\x1c\x1a\x1fh]\x10\x17\x1bH-\x14\x11l\x1cL{\x16\x03,7"f;\x1256"\x04o94_2J\x1cCNiKn<LgMHnS:I\x10K\x05\x065\x00\r\\\x07.\x7f\x0f<\x01\x07^PX\x11h\x08SQL10\x1fRo$Y\x7fu\x16}\x13\x1b5;T\x10\x12\x0el\x7fe\x11^[4\x1d*6%e!IL\x18h3Z_\'\x19\x1cr\x19K4vHt@A\n\x0f#;kiL>75%#\tK-\x0b+\x02\rRY0\x11\x1a=\x0flHLwiW\x08AUuN'
-
 
 def split_ciphertext_in_blocks(ciphertext: bytes) -> list[bytes]:
     ciphertext_sz = len(ciphertext)
     assert ciphertext_sz % BLOCK_SZ == 0
-    return [ciphertext[i : i + BLOCK_SZ] for i in range(0, ciphertext_sz, 16)]
+    return [ciphertext[i: i + BLOCK_SZ] for i in range(0, ciphertext_sz, 16)]
 
 
 def get_ind(char: int) -> int:
@@ -73,10 +71,16 @@ def decrypt_text(ciphertext: bytes, iv: bytes, key: int) -> str:
     plaintext_blocks = [prettify(decrypt_block(iv, ciphertext_blocks[0], key))]
     for i in range(1, blocks_cnt):
         plaintext_blocks.append(
-            prettify(decrypt_block(ciphertext_blocks[i - 1], ciphertext_blocks[i], key))
+            prettify(decrypt_block(
+                ciphertext_blocks[i - 1], ciphertext_blocks[i], key))
         )
     return "".join(plaintext_blocks)
 
 
 if __name__ == "__main__":
-    print(decrypt_text(ciphertext, b"12" * 8, 13))
+
+    ciphertext = b'.r;oms\x02\x07\x15\x1e\x15W8):+\tVQ94\neZ\x03AsIjsH\x14\x1d@iq\'<2"B\x16Mx,\x03.\x0f:1`K;V:uu\x1f\x13Dbz\x11h#o\x1dV!\r|9\'\x1a\x19\x10xt.\x10\x04SHN\x1cvb7K\x082UG-F\x11U)%IF}=l\x02OR_Kg(;Te5(\tV]&\t!\x1e/,|\\:e~e%R\x04\x1blalWy7wWg9*\x0b\x11\x1b\x17\x05|C|\x05\x0fY:\x114\x1aKTVOwFmp\\e\x11\'3awc\x049{[G<b\x12<\x04\x11$RESE={\x0co\x19\x0f\'/\x1c.F"JC8-pe)7\x1em1At0YG\x0e%PiS-U\x06\x00R!A\x087n\x1dgL=RA\x1f\x0c~"H6BbQB[\x15Yb'
+    key = 13
+    iv = b"0123456789ABCDEF"
+
+    print(decrypt_text(ciphertext, iv, key))
--- encrypt.py
+++ encrypt.py
@@ -90,9 +90,7 @@ def encrypt_text(text: str, key: int, iv: bytes) -> bytes:
 
 
 if __name__ == "__main__":
-    text = """ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher that replaces a letter with the 13th letter after it in the alphabet. ROT13 is a special case of the Caesar cipher which was developed in ancient Rome.
-Because there are 26 letters (2×13) in the basic Latin alphabet, ROT13 is its own inverse; that is, to undo ROT13, the same algorithm is applied, so the same action can be used for encoding and decoding. The algorithm provides virtually no cryptographic security, and is often cited as a canonical example of weak encryption.[1]
-ROT13 is used in online forums as a means of hiding spoilers, punchlines, puzzle solutions, and offensive materials from the casual glance. ROT13 has inspired a variety of letter and word games online, and is frequently mentioned in newsgroup conversations. """
+    text = """ROT13 ("rotate by 13 places", sometimes hyphenated ROT-13) is a simple letter substitution cipher that replaces a letter with the 13th letter after it in the alphabet. ROT13 is a special case of the Caesar cipher which was developed in ancient Rome."""
     key = 13
     iv = b"0123456789ABCDEF"
     print(encrypt_text(text, key, iv))
